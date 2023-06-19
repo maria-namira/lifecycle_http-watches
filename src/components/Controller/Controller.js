7ce84e58e971bcb90b8244ddb0a1af2e6990587c
AppVeyr <ci@appveyor.com>
2023-06-19 20:46:10 +0300

    sdf

--- src/components/Controller/Controller.js
+++ src/components/Controller/Controller.js
@@ -1,17 +1,34 @@
 import AddForm from "../AddForm/AddForm";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import WatchList from "../WatchList/WatchList";
 
 
 export default function Controller() {
   const [list, setList] = useState([]);
 
+  useEffect(() => {
+    console.log('useEffect list', list);
+    
+  }, [list])
+  
+
   const addWatch = (watch) => {
+    console.log('watch', watch);
     setList((prevList) => [...prevList, watch]);
   }
 
   const onDelete = (id) => {
-    setList(list.filter((el) => el.id !== id))
+    // console.log('onDelete list', list);
+    // console.log('id', id);
+    
+    
+    setList((prev )=>{
+      console.log('prev', prev);
+      const newArr = prev.filter((el) => el.id !== id)
+      console.log('newArr', newArr);
+      return newArr
+
+    })
   }
   
   return (
